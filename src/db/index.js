3fb7aca9059385612371aca8bda567c531e0b0ab
Pankaj kumar Jha <pankajjha9630@gmail.com>
2026-04-08 15:20:49 +0530

    CONNECT MONGODB

---
+++ src/db/index.js
@@ -0,0 +1,110 @@
+// import dns from "dns"
+// dns.setDefaultResultOrder("ipv4first")
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const uri = "mongodb+srv://pankaj:pankaj67@cluster0.tzxhbya.mongodb.net";
+
+    console.log("USING URI:", uri);
+
+    const conn = await mongoose.connect(uri);
+
+    console.log("MONGODB CONNECTED:", conn.connection.host);
+  } catch (err) {
+    console.error("ERROR:", err);
+  }
+};
+
+export default connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "../constants.js"
+
+// const connectDB=async()=>{
+//     try{
+//         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//             console.log(`\n MONGODB connected !! DB HOST: ${connectionInstance.connection.host}`);
+            
+        
+
+//     }catch (error){
+//         console.log("MONGODB connection FAHH",error);
+//         process.exit(1)
+        
+//     }
+// }
+// export default connectDB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  For normal use in main index.js file xd
+import express from "express"
+const app=express()
+
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.
+            MONGODB_URI}/${DB_NAME}`)
+            app.on("error",(error)=>{
+                console.log("ERRR:",error);
+                throw error
+            })
+            app.listen(process.env.PORT,()=>{
+               console.log(`App is listening on port $ {process.env.PORT}`)
+            })
+    }   catch (error){
+            console.error("ERROR:",error)
+            throw error
+    }
+})*/
---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+import  dotenv  from "dotenv";
+dotenv.config()
+
+
+
+import connectDB from "./db/index.js";
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
