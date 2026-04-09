819cee1c4f9b8b0b7b4cbf31ec0f7d8ab099e5e1
Pankaj kumar Jha <pankajjha9630@gmail.com>
2026-04-09 21:40:21 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ import connectDB from "./db/index.js";
 
 connectDB()
 
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at Port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB Connection failed!!!",err);
+    
+})
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiErro extends Error{
+    constructor(
+        statuscode,
+        message="Something went wrong",
+        errors=[],
+        statck=""
+    ){
+        super(message)
+        this.statuscode=statuscode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=errors
+
+        if(statck){
+            this.statck=this.statck
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+
+    }
+}
+export {ApiErro}
