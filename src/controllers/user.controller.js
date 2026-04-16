644e59dec80c0b49b535dbd746814c3d5b8f650c
Pankaj kumar Jha <pankajjha9630@gmail.com>
2026-04-16 14:49:15 +0530

    Implement user authentication with login, logout, and token refresh functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,26 @@ import{ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.models.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+
+const generateAccessTokenAndRefreshToken=async(userId)=>{
+  try {
+    const user = await User.findById(userId)
+    const accessToken=user.generateAccessToken()
+    const refreshToken=user.generateRefreshToken()
+   
+    user.refreshToken=refreshToken
+    await user.save({validateBeforeSave:false})
+    return {accessToken,refreshToken}
+
+  } catch (error) {
+    throw new ApiError(500,"Something went wrong while generating access and refresh token")
+  }
+} 
+
+
+
+
+
 const registerUser=asyncHandler(async(req,res)=>{
         //get user details from frontend
         //validation -not empty
@@ -14,7 +34,7 @@ const registerUser=asyncHandler(async(req,res)=>{
         /// check for user creation 
         //return res
         const {fullName,email,username,password}=req.body
-        console.log("email:",email);
+      
         
     if  (
         [fullName,email,username,password].some((field)=>field?.trim()==="")
@@ -124,7 +144,131 @@ if (!avatar?.url) {
 
 
 })
+const loginUser=asyncHandler(async (req,res)=>{
+  //req body->data
+  //username or email
+  //find the user
+  //password check
+  //access and refersh token
+  //send cookie
+const {email,username,password}=req.body
+console.log(email);
+
+if (!(username || email)) {
+   throw new ApiError(400,"username or email is required")
+}
+
+const user=await User.findOne({
+  $or:[{username},{email}]
+})
+
+if (!user) {
+  throw new ApiError(404,"User does not exist")
+}
+
+const isPasswordValid=await user.isPasswordCorrect(password)
+
+if (!isPasswordValid) {
+  throw new ApiError(401,"Invalid User credeantials")
+}
+const {accessToken,refreshToken}=await generateAccessTokenAndRefreshToken(user._id)
+
+const logggedInUser=await User.findById(user._id).select("-password -refreshToken")
+
+const options ={
+  httpOnly:true,
+  secure:true
+}
+return res
+.status(200)
+.cookie("accessToken", accessToken,options)
+.cookie("refreshToken",refreshToken,options)
+.json(
+  new ApiResponse(
+    200,
+    {
+    user:logggedInUser,accessToken,refreshToken
+  },
+  "USer logged in Successfully"
+)
+)
+})
+
+
+const logoutUser=asyncHandler(async(req,res)=>{
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken:undefined
+      }
+    },
+    {
+    returnDocument: "after"
+   }
+  )
+  const options ={
+  httpOnly:true,
+  secure:true
+}
+ return res
+ .status(200)
+ .clearCookie("accessToken",options)
+ .clearCookie("refreshToken",options)
+ .json(new ApiResponse(200,{},"User logged Out successfully"))
+})
+const refreshAccessToken=asyncHandler(async(req,res)=>{
+  const incomingRefershToken=req.cookie.refreshToken || req.body.refreshToken
+  if (!incomingRefershToken){
+    throw new ApiError(401,"Unauthorized request")
+  }
+
+try {
+  const decodedToken=jwt.verify(
+    incomingRefershToken,
+    process.env.REFRESH_TOKEN_SECRET
+  )
+  const user=await User.findById(decodedToken?._id)
+  
+  if(!user){
+    throw new ApiError(401,"Invalid Refresh Token")
+  }
+  if (incomingRefershToken !==user?.refreshToken){
+    throw new ApiError(401,"Refresh token is expired or used")
+  }
+  
+  const {accessToken,newRefreshToken} = await generateAccessTokenAndRefreshToken(user._id)
+  
+  const options ={
+    httpOnly:true,
+    secure:true
+  }
+  return res
+  .status(200)
+  .cookie("accessToken",accessToken,options)
+  .cookie("refreshToken",newRefreshToken,options)
+  .json(
+    new ApiResponse(
+      200,
+      {accessToken,refreshToken:newRefreshToken},
+      "Access token refreshed"
+    )
+  )
+} catch (error) {
+    throw new ApiError(401,error?.message || "Invalid refresh token")
+}
 
 
 
-export {registerUser}
+})
+
+
+
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  
+}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,3 +1,5 @@
+// import dotenv from "dotenv";
+// dotenv.config();
 // import {v2 as cloudinary} from "cloudinary"
 // import fs from "fs"
 
@@ -37,9 +39,7 @@ dotenv.config();
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-console.log("cloud name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("api key:", process.env.CLOUDINARY_API_KEY);
-console.log("api secret exists:", !!process.env.CLOUDINARY_API_SECRET);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
